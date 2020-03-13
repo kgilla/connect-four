bfedf95e4a57130dd595850e8da7d9fe50a97da0
kenneth <gilmorekenneth@gmail.com>
2020-03-13 16:30:50 -0400

    Fixed up a few things and refactored code down

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,12 +1,9 @@
 class GameBoard
-  attr_accessor :board, :player_one_turn
-  attr_reader :winning_combos, :player_one_symbol, :player_two_symbol
-
   def initialize
     @board = make_board
-    @p1_symbol = "X"
-    @p2_symbol = "O"
-    @turn = who_first
+    @p1_symbol = "☻"
+    @p2_symbol = "☺"
+    @turn = rand(1..2)
     @winning_combos = [[0,1],[0,2],[0,3]], [[1,0],[2,0],[3,0]], 
     [[1,1],[2,2],[3,3]], [[1,-1],[2,-2],[3,-3]]
   end
@@ -16,11 +13,6 @@ class GameBoard
     7.times { board << Array.new(6,"_") }
     board
   end
-
-  def who_first
-    first_turn = rand(1..2)
-    return first_turn == 1 ? 1 : 2
-  end 
 
   def get_move
     puts "\nPlayer #{@turn}!, choose a column from 1-7 to drop your piece"
@@ -39,33 +31,28 @@ class GameBoard
     until @board[col][row] == "_"
       row += 1
     end
-    if @turn == 1
-      @board[col][row] = @p1_symbol
-    elsif @turn == 2
-      @board[col][row] = @p2_symbol
-    end
+    @board[col][row] = @p1_symbol if @turn == 1
+    @board[col][row] = @p2_symbol if @turn == 2
   end
 
   def print_board
-    puts "\n" + "*"*10 + "Choose A Number!" + "*"*9
-    puts "| 1 || 2 || 3 || 4 || 5 || 6 || 7 |"
+    puts "\n" + "*"*5 + "  Choose A Number  " + "*"*5
+    puts "|~1~|~2~|~3~|~4~|~5~|~6~|~7~|"
     (5).downto(0).each do |j|
-      puts 
+      print "\n|   |   |   |   |   |   |   |\n" 
       (0).upto(6).each do |i|
-        print "|_#{@board[i][j]}_|" 
+        print "|_#{@board[i][j]}_" if i < 6
+        print "|_#{@board[i][j]}_|" if i == 6 
       end
     end
-    puts "\n" + "#"*35 + "\n" + "#"*4 + " "*27 + "#"*4 
+    puts "\n" + "#"*29 + "\n" + "#"*4 + " "*21 + "#"*4 
   end
 
   def scan_board
     @board.each_with_index do |col, col_i|
       col.each_with_index do |row, row_i|
-        if @board[col_i][row_i] == @p1_symbol && @turn == 1 
-          check_win(col_i,row_i, @p1_symbol)
-        elsif @board[col_i][row_i] == @p2_symbol && @turn == 2
-          check_win(col_i,row_i, @p2_symbol)
-        end
+        check_win(col_i,row_i, @p1_symbol) if row == @p1_symbol && @turn == 1 
+        check_win(col_i,row_i, @p2_symbol) if row == @p2_symbol && @turn == 2
       end
     end
   end
@@ -73,87 +60,74 @@ class GameBoard
   def check_win (x,y,symbol)
     @winning_combos.each do |combo|
       count = 0
-      combo.each do |xy|
-        cord = x + xy[0], y + xy[1] 
+      combo.each do |combo_cords|
+        cord = x + combo_cords[0], y + combo_cords[1] 
         if cord[0] >= 0 && cord[0] < 7 && cord[1] >= 0 && cord[1] < 6
-          if @board[cord[0]][cord[1]] == symbol
-          count += 1
+          count += 1 if @board[cord[0]][cord[1]] == symbol
           declare_winner if count == 3
-          end
         end
       end
     end
   end
 
   def declare_winner 
-    puts "Congrats player #{@turn}!"
-    puts "Would you like to play again? (Y/N)"
+    puts "\n!!!!! WINNER! WINNER! WINNER! WINNER! !!!!!"
+    puts "\nCongrats Player #{@turn}!", "\nWould you like to play again? (Y/N)"
     answer = gets.chomp.upcase
-    if answer == "Y"
-      new_game = Main.new
-    else
-      puts "goodbye"
-      exit
-    end
+    new_game = Main.new if answer == "Y"
+    exit
   end
 
   def change_turn
-    if @turn == 1
-      @turn = 2
-    else 
-      @turn = 1
-    end
+    return @turn == 1 ? @turn = 2 : @turn = 1
   end
 end
 
 class Main
-  attr_accessor :game
-
+  attr_accessor :game, :turn_count 
+  
   def initialize 
     @game = GameBoard.new
-    header
+    @turn_count = 0
+    game_loop
   end
 
   def game_loop
     intro
-    turn = 0
-    while turn < 42
+    while @turn_count < 42
       game.get_move
       game.print_board
       game.scan_board
       game.change_turn
+      @turn_count += 1
     end
+    tie_game
   end
 
-  def header
-    puts "$"*80
-    puts "#"*25 + "   C O N N E C T  F O U R !   " + "#"*25
-    puts "$"*80
-    title_screen
-  end
-
-  def title_screen
-    puts "\n1) Start a new game"
-    puts "\n2) Info"
-    puts "\n3) Exit"
-    answer = gets.chomp.to_i
-    if answer == 1
-      game_loop
-    else 
-      exit
-    end
-  end
-  
   def intro
-    puts "\nWelcome to Connect Four!"
-    puts "\ndecide who should be player one and who will be player two"
+    puts "\nNow Get ready to Connect Four!","\nLets decide who will go first..."
     sleep(1)
-    puts "Okay lets decide who will go first..."
-    sleep(1)
+    game.print_board
+  end
+
+  def tie_game
+    puts "Wow you both both lost...", "Want to play again? (Y/N)"
+    answer = gets.chomp.upcase
+    new_game = Main.new if answer == "Y"
+    exit
   end
 end
 
-new_game = Main.new
+def header
+  puts "☻"*80, "☺"*25 + "   C O N N E C T  F O U R !   " + "☺"*25, "☻"*80
+  title_screen
+end
 
+def title_screen
+  puts "\n1) Start a new game", "\n2) Exit"
+  answer = gets.chomp.to_i
+  new_game = Main.new if answer == 1
+  exit
+end
 
-  
+header
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -3,17 +3,25 @@ require "./lib/connect_four"
 describe GameBoard do
   
   describe "#make_board" do
-    xit "makes a new game board consisting of 7 arrays of size 6" do
-      expect()
+    it "makes a new game board consisting of 7 arrays of size 6" do
+      game = GameBoard.new
+      expect(game.make_board).to eql([["_","_","_","_","_","_"], ["_","_","_","_","_","_"], ["_","_","_","_","_","_"], ["_","_","_","_","_","_"], ["_","_","_","_","_","_"], ["_","_","_","_","_","_"], ["_","_","_","_","_","_"]])
     end
+  end
 
+  describe "#insert_move" do
+    it "Inserts a players move into the correct spot of the game board" do
+      game = GameBoard.new
+      expect(game.insert_move(1)).to eql("X")
+    end
   end
 end
 
 describe Main do 
 
   describe "#player_turn" do
-    it "asks player for input and returns input as variable" do
+    xit "asks player for input and returns input as variable" do
       expect(player_turn)
     end
   end
+end
